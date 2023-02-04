e0c7fac499990657e31523dfd5e84f0f403d4f11
shanziii <shanziiihalstead@gmail.com>
2023-02-03 20:46:24 -0500

    Added Login Page and CSS Styling to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,46 @@
-<nav>
-    <a href="/">Home Page</a>
-    <a href="/quiz">Quiz Page</a>
-    <a href="/results">Results Page</a>
 
-</nav>
+
+<!-- CSS Styling extracted from codepen-->
+<style> 
+    body {
+        background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        background-size: 400% 400%;
+        animation: gradient 15s ease infinite;
+        height: 100vh;
+    }
+
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }   
+
+    nav{
+
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-size: large;
+        background-color: beige;
+        padding-inline: 50px;
+        float:inherit;
+    }
+
+</style> 
+<body>
+    <nav>
+        <a href="/">Home Page</a>
+        <a href="/login">Login Page</a>
+        <a href="/quiz">Quiz Page</a>
+        <a href="/results">Results Page</a>
+    
+    </nav>
 
 <slot>
 
 </slot>
+</body>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts" >
+	import Layout from "../+layout.svelte";
+    let username = "";
+    let password = ""; 
+
+    function submit(password: string){
+
+     alert("Your password " + password + " is acceptible");
+
+    }
+
+</script>
+
+<body>
+
+    <div class="d-flex flex-column justify-content-center w-600 h-800">
+        <form>
+                <label for="username"> Username:</label> <br>
+                <input type="text" bind:value={username}/><br>
+    
+                <label for="password"> Password:</label> <br>
+                <input type="password" bind:value={password}/><br><br>
+
+                <button type="submit" on:click={submit(password)}>Login</button>
+    
+        </form>
+        
+    </div>
+
+    <div style="background-color:white; ">
+        <!-- svelte-ignore empty-block -->
+        {#if username == ""}     
+               
+        {:else}
+            <h1 style="color:black;"> Welcome {username}</h1>
+
+        {/if}
+
+    </div>
+</body>
+
+
